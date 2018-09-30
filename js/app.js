677eb173046d0f02d680b8652f92df7e1724b703
Alexis Texis <alextexis@hotmail.com>
2018-09-30 14:46:25 -0500

    Puliendo detalles

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const nav = document.getElementById('nav');
 const iconMenu = document.getElementById('iconMenu');
 const windowResize = window.matchMedia('screen and (max-width:767px)');
 windowResize.addListener(EventTouch);
+
 function EventTouch(event)
 {
   if(event.matches)
