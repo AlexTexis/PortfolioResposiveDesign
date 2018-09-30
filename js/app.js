81c54eada210bd4d2af5a468575e0510cbf32d73
Alexis Texis <alextexis@hotmail.com>
2018-09-30 11:20:54 -0500

    Aplicando Responsive y js

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const nav = document.getElementById('nav');
+const iconMenu = document.getElementById('iconMenu');
+const windowResize = window.matchMedia('screen and (max-width:767px)');
+windowResize.addListener(EventTouch);
+function EventTouch(event)
+{
+  if(event.matches)
+  {
+    iconMenu.addEventListener('touchstart',toggleMenu);
+  }
+  else
+  {
+    iconMenu.removeEventListener('touchstart',toggleMenu);
+
+  }
+}
+
+function toggleMenu()
+{
+  nav.classList.toggle('show');
+}
